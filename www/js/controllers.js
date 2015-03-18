2c1cf4944c8946e894c0a295d7aa2dc39712d0c6
GYB <gyb314@gmail.com>
2015-03-18 00:19:04 -0700

    adding controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -14,7 +14,7 @@ angular.module('starter.controllers', [])
   $scope.pet = PetService.get($stateParams.petId);
 });
 
-// .controller('customersCtrl', function($scope, $http) {
-//   $http.get("http://www.w3schools.com/website/Customers_JSON.php")
-//   .success(function (response) {$scope.names = response;});
-// });
+.controller('customersCtrl', function($scope, $http) {
+  // $http.get("http://www.w3schools.com/website/Customers_JSON.php")
+  // .success(function (response) {$scope.names = response;});
+});
